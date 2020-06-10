bd1d0b0733d1c95a327b20aed93acfcfe69cbb3c
Chris <christophertberg@gmail.com>
2020-06-09 21:47:16 -0700

    Updates

---
+++ precache-manifest.a999be7ee2d4c38582932fde4e5d775c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b55be281965c3c1f9c9feb484c89b6c6",
+    "url": "/bergc-portfolio/index.html"
+  },
+  {
+    "revision": "9015af10fd32c8458388",
+    "url": "/bergc-portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4d195cb40f7b26368813",
+    "url": "/bergc-portfolio/static/js/2.4968d256.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bergc-portfolio/static/js/2.4968d256.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9015af10fd32c8458388",
+    "url": "/bergc-portfolio/static/js/main.8a0da4ce.chunk.js"
+  },
+  {
+    "revision": "cbc6189e1847b300f239",
+    "url": "/bergc-portfolio/static/js/runtime-main.52a0b02c.js"
+  }
+]);

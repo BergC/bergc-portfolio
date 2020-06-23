195d59ffa19be076691764bca14cbd496ca5a7a3
Chris <christophertberg@gmail.com>
2020-06-22 23:49:36 -0700

    began work on the portfolio section

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,8 +1,29 @@
 import React from 'react';
 
 const Portfolio = () => (
-    <div>
-        <p></p>
+    <div className='wrapper'>
+        <div className='portfolio'>
+            <h3 className='portfolio__project'>
+                <sup className='portfolio__project-num'>01</sup>
+                <a href='#' className='portfolio__project-link'>
+                    Snowbound
+                </a>
+            </h3>
+
+            <h3 className='portfolio__project'>
+                <sup className='portfolio__project-num'>01</sup>
+                <a href='#' className='portfolio__project-link'>
+                    Diamonds Discovered
+                </a>
+            </h3>
+
+            <h3 className='portfolio__project'>
+                <sup className='portfolio__project-num'>01</sup>
+                <a href='#' className='portfolio__project-link'>
+                    Expensify
+                </a>
+            </h3>
+        </div>
     </div>
 );
 

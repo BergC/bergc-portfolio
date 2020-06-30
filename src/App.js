8781cb0dc57f3e0c8ea46f11c2ec6ca2fd7c8b8b
Chris <christophertberg@gmail.com>
2020-06-29 23:00:35 -0700

    worked on content transitions

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
+import { Transition } from 'react-transition-group';
 
 import BigHero from './components/BigHero';
 import Contact from './components/Contact';
 import Nav from './components/Nav';
 import Portfolio from './components/Portfolio';
 
+const duration = 300;
+
+const defaultStyle = {
+    transition: `opacity ${duration}ms ease-in-out`,
+    opacity: 0,
+};
+
+const transitionStyles = {
+    entering: { opacity: 1 },
+    entered: { opacity: 1 },
+    exiting: { opacity: 0 },
+    exited: { opacity: 0 },
+};
+
 function App() {
+    const [inProp, setInProp] = useState(false);
+
     return (
         <div className='App'>
             <Nav />
--- src/components/BigHero.js
+++ src/components/BigHero.js
@@ -1,31 +1,33 @@
 import React, { Fragment } from 'react';
 import Typewriter from 'typewriter-effect';
 
-const BigHero = () => (
-    <Fragment>
-        <div className='bighero'>
-            <h1 className='bighero__header'>
-                <Typewriter
-                    options={{
-                        strings: [
-                            'chris berg',
-                            'skiier',
-                            'developer',
-                            'ramen chef',
-                            'beer brewer',
-                            'mountain biker',
-                            'gamer',
-                            'swimmer',
-                        ],
-                        autoStart: true,
-                        loop: true,
-                    }}
-                />
-            </h1>
-        </div>
+const BigHero = () => {
+    return (
+        <Fragment>
+            <div className='bighero'>
+                <h1 className='bighero__header'>
+                    <Typewriter
+                        options={{
+                            strings: [
+                                'chris berg',
+                                'skiier',
+                                'developer',
+                                'ramen chef',
+                                'beer brewer',
+                                'mountain biker',
+                                'gamer',
+                                'swimmer',
+                            ],
+                            autoStart: true,
+                            loop: true,
+                        }}
+                    />
+                </h1>
+            </div>
 
-        <button className='bighero__button'>See Stuff</button>
-    </Fragment>
-);
+            <button className='bighero__button'>See Stuff</button>
+        </Fragment>
+    );
+};
 
 export default BigHero;

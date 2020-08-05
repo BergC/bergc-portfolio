ea256f41e97fddb335ae3f5e4185e8fbcf1258a5
Chris <christophertberg@gmail.com>
2020-08-04 21:13:07 -0700

    updated portfolio section

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -56,23 +56,20 @@ const Portfolio = () => (
 
                 <div className='portfolio__project'>
                     <h3 className='portfolio__project-title'>
-                        <sup className='portfolio__project-num'>03</sup>
+                        <sup className='portfolio__project-num'>04</sup>
                         <a
-                            href='https://bergc-expensify.herokuapp.com/'
+                            href='https://www.codewars.com/users/paosiehf'
                             target='_blank'
                             rel='noopener noreferrer'
                             className='portfolio__project-link'
                         >
-                            Expensify
+                            Code Wars
                         </a>
                     </h3>
 
                     <ul className='portfolio__frameworks'>
-                        <li>Node.js</li>
-                        <li>REACT</li>
-                        <li>Firebase</li>
-                        <li>Express</li>
-                        <li>Sass</li>
+                        <li>JavaScript</li>
+                        <li>Python</li>
                     </ul>
                 </div>
             </div>

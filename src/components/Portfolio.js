6be69c4f72b7b85dcd76a9f32404917230d01d62
Chris <christophertberg@gmail.com>
2020-09-03 16:45:38 -0700

    added descriptions to each portfolio project

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,101 +1,221 @@
 // Import packages.
-import React, { Fragment, useRef, useEffect } from 'react';
+import React, { Fragment, useRef, useEffect, useState } from 'react';
 import { gsap } from 'gsap';
-import Accordion from 'react-bootstrap/Accordion';
-
-// Import components.
-import BigHero from './BigHero';
 
 const Portfolio = () => {
+    // useRef hook used so that our GSAP transitions know what element to apply transitions to.
     const portfolioRefIn = useRef(null);
-    const bigHeroRefIn = useRef(null);
 
+    // useEffect hook used to apply our GSAP transitions.
     useEffect(() => {
         gsap.from(portfolioRefIn.current, 0.8, {
             y: 150,
-            delay: 0.8,
+            delay: 0.7,
             ease: 'power1.out',
             opacity: 0,
         });
     }, []);
 
-    useEffect(() => {
-        gsap.from(bigHeroRefIn.current, 0.8, {
-            y: -100,
-            delay: 0.8,
-            ease: 'power1.out',
-            opacity: 0,
-        });
-    }, []);
+    // useState hook used to display / hide our portfolio accordion.
+    const [isOpen, setOpen] = useState({
+        snowboundOpen: false,
+        diamondsOpen: false,
+        codeWarsOpen: false,
+    });
+
+    const { snowboundOpen, diamondsOpen, codeWarsOpen } = isOpen;
+
+    const onClick = (e) => {
+        e.preventDefault();
+        const target = e.target.innerHTML;
+
+        if (target === 'Snowbound') {
+            setOpen({
+                ...isOpen,
+                snowboundOpen: !snowboundOpen,
+            });
+        } else if (target === 'Diamonds Discovered') {
+            setOpen({
+                ...isOpen,
+                diamondsOpen: !diamondsOpen,
+            });
+        } else if (target === 'Code Wars') {
+            setOpen({
+                ...isOpen,
+                codeWarsOpen: !codeWarsOpen,
+            });
+        }
+    };
 
     return (
         <Fragment>
-            <div ref={bigHeroRefIn}>
-                <BigHero />
-            </div>
-
-            <div ref={portfolioRefIn} id='portfolio__flex-wrapper'>
+            <div ref={portfolioRefIn}>
                 <div className='portfolio__project'>
-                    <h3 className='portfolio__project-title'>
+                    <h3 onClick={onClick}>
                         <sup className='portfolio__project-num'>01</sup>
-                        <a
-                            href='https://bergc-snowbound.herokuapp.com/'
-                            target='_blank'
-                            rel='noopener noreferrer'
-                            className='portfolio__project-link'
+                        <span
+                            className={
+                                snowboundOpen
+                                    ? 'portfolio__project-title--open'
+                                    : 'portfolio__project-title'
+                            }
                         >
                             Snowbound
-                        </a>
+                        </span>
                     </h3>
 
-                    <ul className='portfolio__frameworks'>
-                        <li>Node.js</li>
-                        <li>REACT</li>
-                        <li>MongoDB</li>
-                        <li>Express</li>
-                        <li>Sass</li>
-                    </ul>
+                    {snowboundOpen ? (
+                        <div className='portfolio__project-detail-wrapper'>
+                            <div className='portfolio__project-detail'>
+                                <h4>Purpose</h4>
+                                <p>
+                                    Fetch the snow and weather conditions for
+                                    nearby mountains.
+                                </p>
+                            </div>
+
+                            <div className='portfolio__project-detail'>
+                                <h4>Inspiration</h4>
+                                <p>
+                                    I'm an avid skiier and have found that
+                                    Washington has so many mountains in driving
+                                    distance. I always disliked having to Google
+                                    the conditions at every accessible mountain
+                                    individually, so I decided to built a tool
+                                    that would compile weather and snow data in
+                                    a single location.
+                                </p>
+                            </div>
+
+                            <div className='portfolio__project-detail'>
+                                <h4>Tools</h4>
+                                <p>
+                                    Node.js | React.js | GSAP | Bootstrap |
+                                    Express | MongoDB | mLab | Heroku | Sass |
+                                    Mongoose | Axios | Redux | Font Awesome
+                                </p>
+                            </div>
+
+                            <a
+                                href='https://bergc-snowbound.herokuapp.com/'
+                                target='_blank'
+                                rel='noopener noreferrer'
+                            >
+                                <button className='bighero__button'>Go</button>
+                            </a>
+                        </div>
+                    ) : null}
                 </div>
 
                 <div className='portfolio__project'>
-                    <h3 className='portfolio__project-title'>
+                    <h3 onClick={onClick}>
                         <sup className='portfolio__project-num'>02</sup>
-                        <a
-                            href='https://bergc-diamondcost.herokuapp.com/'
-                            target='_blank'
-                            rel='noopener noreferrer'
-                            className='portfolio__project-link'
+                        <span
+                            className={
+                                diamondsOpen
+                                    ? 'portfolio__project-title--open'
+                                    : 'portfolio__project-title'
+                            }
                         >
                             Diamonds Discovered
-                        </a>
+                        </span>
                     </h3>
 
-                    <ul className='portfolio__frameworks'>
-                        <li>Node.js</li>
-                        <li>REACT</li>
-                        <li>MongoDB</li>
-                        <li>Express</li>
-                        <li>Sass</li>
-                    </ul>
+                    {diamondsOpen ? (
+                        <div className='portfolio__project-detail-wrapper'>
+                            <div className='portfolio__project-detail'>
+                                <h4>Purpose</h4>
+                                <p>
+                                    Fetch diamond prices from multiple sources
+                                    based upon user provided criteria.
+                                </p>
+                            </div>
+
+                            <div className='portfolio__project-detail'>
+                                <h4>Inspiration</h4>
+                                <p>
+                                    I recently proposed to my girlfriend, but
+                                    before proposing I had no idea how to buy a
+                                    ring. More importantly, I had no idea how to
+                                    buy a ring efficiently. I wanted the highest
+                                    quality at the lowest cost. After reading up
+                                    on the 4c's and snooping around several big
+                                    name online wholesalers, I found there were
+                                    discrepancies in price across the platforms
+                                    despite displaying similarly graded
+                                    diamonds. I wanted to be able to search them
+                                    all simultaneously and figure out which was
+                                    best priced.
+                                </p>
+                            </div>
+
+                            <div className='portfolio__project-detail'>
+                                <h4>Tools</h4>
+                                <p>
+                                    Node.js | React.js | Express | MongoDB |
+                                    mLab | Heroku | Sass | Node Fetch | Axios |
+                                    Mongoose | jQuery | Redux | Moment | Font
+                                    Awesome
+                                </p>
+                            </div>
+
+                            <a
+                                href='https://bergc-diamondcost.herokuapp.com/'
+                                target='_blank'
+                                rel='noopener noreferrer'
+                            >
+                                <button className='bighero__button'>Go</button>
+                            </a>
+                        </div>
+                    ) : null}
                 </div>
 
                 <div className='portfolio__project'>
-                    <h3 className='portfolio__project-title'>
-                        <sup className='portfolio__project-num'>04</sup>
-                        <a
-                            href='https://www.codewars.com/users/paosiehf'
-                            target='_blank'
-                            rel='noopener noreferrer'
-                            className='portfolio__project-link'
+                    <h3 onClick={onClick}>
+                        <sup className='portfolio__project-num'>03</sup>
+                        <span
+                            className={
+                                codeWarsOpen
+                                    ? 'portfolio__project-title--open'
+                                    : 'portfolio__project-title'
+                            }
                         >
                             Code Wars
-                        </a>
+                        </span>
                     </h3>
 
-                    <ul className='portfolio__frameworks'>
-                        <li>JavaScript</li>
-                        <li>Python</li>
-                    </ul>
+                    {codeWarsOpen ? (
+                        <div className='portfolio__project-detail-wrapper'>
+                            <div className='portfolio__project-detail'>
+                                <h4>Purpose</h4>
+                                <p>Practice my basic coding skills.</p>
+                            </div>
+
+                            <div className='portfolio__project-detail'>
+                                <h4>Inspiration</h4>
+                                <p>
+                                    I wanted to make sure that I was taking time
+                                    to challenge myself with problems created by
+                                    others. I also wanted to ensure that I
+                                    didn't forget what I was learning, after all
+                                    practice makes perfect.
+                                </p>
+                            </div>
+
+                            <div className='portfolio__project-detail'>
+                                <h4>Tools</h4>
+                                <p>JavaScrtipt | Python</p>
+                            </div>
+
+                            <a
+                                href='https://www.codewars.com/users/paosiehf'
+                                target='_blank'
+                                rel='noopener noreferrer'
+                            >
+                                <button className='bighero__button'>Go</button>
+                            </a>
+                        </div>
+                    ) : null}
                 </div>
             </div>
         </Fragment>
